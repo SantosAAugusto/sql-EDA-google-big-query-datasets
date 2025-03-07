aaa64a31ee01a57214fc43411d7bf7cea545575e
Antonio Augusto C. Santos <163574054+SantosAAugusto@users.noreply.github.com>
2025-03-07 17:48:37 -0300

    Update EDA-queries.sql

--- datasets/thelook_ecommerce/EDA-queries.sql
+++ datasets/thelook_ecommerce/EDA-queries.sql
@@ -1,52 +1,52 @@
-/* 1) Utilize o dataset público “thelook_ecommerce”, que está presente no 
+/* Utilize o dataset público “thelook_ecommerce”, que está presente no 
 projeto “bigquery-public-data” para trazer o identificador único dos usuários, o primeiro nome, 
 email e idade, ordernados pela data de criação do cadastro. */
-select 
+SELECT 
 id, 
 first_name, 
 email, 
 age 
-from `bigquery-public-data.thelook_ecommerce.users` 
-order by created_at; 
+FROM `bigquery-public-data.thelook_ecommerce.users` 
+ORDER BY created_at; 
 
 
-/* 2) No mesmo dataset do exercício anterior, extraia os pedidos que 
+/* No mesmo dataset do exercício anterior, extraia os pedidos que 
 tenham status “Shipped” ou “Processing”. */ 
-select 
+SELECT 
 order_id, 
 status 
-from `bigquery-public-data.thelook_ecommerce.orders` 
-where status = "Shipped" or status = "Processing"; 
-select 
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+WHERE status = "Shipped" OR status = "Processing"; 
+SELECT 
 order_id, 
 status 
-from `bigquery-public-data.thelook_ecommerce.orders` 
-where status in ("Shipped", "Processing"); 
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+WHERE status IN ("Shipped", "Processing"); 
 
 
-/* 3) Considerando o mesmo exercício anterior, traga os pedidos que foram 
+/* Considerando o mesmo exercício anterior, traga os pedidos que foram 
 criados no dia “2022-04-05” e, ao mesmo tempo, tenham status “Cancelled”. */ 
-select 
+SELECT 
 order_id, 
 status, 
 created_at, 
-date(created_at) as dia 
-from `bigquery-public-data.thelook_ecommerce.orders` 
-where status = "Cancelled" and date(created_at) = "2022-04-05"; 
+date(created_at) AS dia 
+FROM `bigquery-public-data.thelook_ecommerce.orders` 
+WHERE status = "Cancelled" AND date(created_at) = "2022-04-05"; 
 
 
-/* 4) O time de marketing deseja fazer uma campanha em um país específico, com o mesmo dataset dos itens anteriores.  
+/* O time de marketing deseja fazer uma campanha em um país específico, com o mesmo dataset dos itens anteriores.  
 Para isso, eles te pedem os e-mails de todos os clientes que moram no Brasil. */ 
-select 
+SELECT 
 email 
-from `bigquery-public-data.thelook_ecommerce.users` 
-where email is not null and country = "Brasil"; 
+FROM `bigquery-public-data.thelook_ecommerce.users` 
+WHERE email IS NOT NULL AND country = "Brasil"; 
 
 
-/* 5) No mesmo dataset anterior, traga o nome dos usuários e o id de seus pedidos. */ 
-select 
+/* No mesmo dataset anterior, traga o nome dos usuários e o id de seus pedidos. */ 
+SELECT 
 order_id, 
 first_name 
-from `bigquery-public-data.thelook_ecommerce.users` u  
-join `bigquery-public-data.thelook_ecommerce.orders` o on u.id = 
+FROM `bigquery-public-data.thelook_ecommerce.users` u  
+JOIN `bigquery-public-data.thelook_ecommerce.orders` o ON u.id = 
 o.user_id;
